b3415e0b95e00d184842eb5ceee7a6c5b2597597
Aditya Mehra <Aix.m@outlook.com>
2019-12-13 03:28:54 +1030

    switch to event type from utterance

--- bin/mycroft-skill-launcher.py
+++ bin/mycroft-skill-launcher.py
@@ -2,18 +2,13 @@
 
 import sys
 import json
-#import notify2
 from websocket import create_connection
 
-#notify2.init("mycroftlauncher")
-#n = notify2.Notification('foo', 'bar')
-#n.show()
-utterance = sys.argv[1]
+event_type = sys.argv[1]
 ws = create_connection("ws://0.0.0.0:8181/core")
 msg = {}
-msg["type"] = "recognizer_loop:utterance"
+msg["type"] = event_type
 msg["data"] = {}
-msg["data"]["utterances"] = [utterance]
 msg_request = json.dumps(msg)
 ws.send(msg_request)
 sys.exit()
