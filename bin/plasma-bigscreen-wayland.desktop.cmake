142e96b4cbab5c88921d3bba0bd26d930a366625
Bart Ribbers <bribbers@disroot.org>
2020-09-08 16:02:15 +0200

    Install a startup script
    This creates a /usr/share/wayland-sessions/plasma-bigscreen.desktop
    launcher which can be used to launch Plasma Bigscreen in combination
    with /usr/bin/plasma-bigscreen

---
+++ bin/plasma-bigscreen-wayland.desktop.cmake
@@ -0,0 +1,7 @@
+[Desktop Entry]
+Exec=dbus-run-session ${CMAKE_INSTALL_FULL_BINDIR}/plasma-bigscreen-wayland
+TryExec=${CMAKE_INSTALL_FULL_BINDIR}/plasma-bigscreen-wayland
+DesktopNames=KDE
+Name=Plasma Bigscreen
+Comment=Plasma Bigscreen by KDE
+X-KDE-PluginInfo-Version=${PROJECT_VERSION}
