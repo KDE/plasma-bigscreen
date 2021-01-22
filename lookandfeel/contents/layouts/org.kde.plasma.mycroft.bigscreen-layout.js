0ef6a87ac84981c2dfd1c176f4d6683b7185ac19
Marco Martin <notmart@gmail.com>
2021-01-22 11:31:13 +0100

    iconvenrt shell/lnf to SPDX

--- lookandfeel/contents/layouts/org.kde.plasma.mycroft.bigscreen-layout.js
+++ lookandfeel/contents/layouts/org.kde.plasma.mycroft.bigscreen-layout.js
@@ -1,3 +1,8 @@
+/*
+    SPDX-FileCopyrightText: 2020 Marco Martin <mart@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
 
 var desktopsArray = desktopsForActivity(currentActivity());
 for (var j = 0; j < desktopsArray.length; j++) {
