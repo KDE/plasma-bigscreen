3841db90f1899d8a90e0a36dc1ba05cdefaea900
Bart Ribbers <bribbers@disroot.org>
2021-12-22 14:10:42 +0100

    Install a startup script for X11-based sessions
    It seems the X11 session is still going to be used for a while on KDE
    Neon and Manjaro. Let's at least make sure both X11 and Wayland are
    launched the same way

---
+++ bin/plasma-bigscreen-x11.desktop.cmake
@@ -0,0 +1,7 @@
+[Desktop Entry]
+Exec=dbus-run-session ${CMAKE_INSTALL_FULL_BINDIR}/plasma-bigscreen-x11
+TryExec=${CMAKE_INSTALL_FULL_BINDIR}/plasma-bigscreen-x11
+DesktopNames=KDE
+Name=Plasma Bigscreen (X11)
+Comment=Plasma Bigscreen by KDE (X11)
+X-KDE-PluginInfo-Version=${PROJECT_VERSION}
