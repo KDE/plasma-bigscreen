5522d6d6ec9face176c37b0f33247b49c7bd179b
Devin Lin <espidev@gmail.com>
2025-08-10 15:52:02 -0400

    Add missing Messages.sh

--- containments/homescreen/package/Messages.sh
+++ containments/homescreen/package/Messages.sh
@@ -4,4 +4,4 @@
 # SPDX-FileCopyrightText: 2019 Marco Martin <mart@kde.org>
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-$XGETTEXT `find . -name '*.qml'` -o $podir/plasma_applet_org.kde.bigscreen.homescreen.pot
+$XGETTEXT `find . -name \*.cpp -o -name \*.h -o -name \*.qml -o -name \*.js` -o $podir/plasma_applet_org.kde.bigscreen.homescreen.pot
---
+++ kcms/bluetooth/Messages.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2025 Devin Lin <devin@kde.org>
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+$XGETTEXT `find . -name \*.cpp -o -name \*.h -o -name \*.qml -o -name \*.js` -o $podir/kcm_mediacenter_bluetooth.pot
