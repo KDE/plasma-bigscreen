ab3322e3a420105509de591cf89424b15062fb81
l10n daemon script <scripty@kde.org>
2026-01-25 01:39:12 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- inputhandler/org.kde.plasma.bigscreen.inputhandler.desktop.cmake
+++ inputhandler/org.kde.plasma.bigscreen.inputhandler.desktop.cmake
@@ -2,8 +2,10 @@
 # SPDX-FileCopyrightText: 2022 Bart Ribbers <bribbers@disroot.org>
 [Desktop Entry]
 Name=Plasma Bigscreen Input Handler
+Name[ar]=معالج إدخال لبلازما الشاشة الكبيرة
 Name[ca]=Gestor d'entrada del Plasma Bigscreen
 Name[ca@valencia]=Gestor d'entrada de Plasma Bigscreen
+Name[es]=Controlador de entrada de Plasma Bigscreen
 Name[fr]=Gestionnaire d'entrées pour Plasma Bigscreen
 Name[he]=מטפל הקלט לפלזמה למסכים גדולים
 Name[ia]=MAneator de ingresso de Plasma Bigscreen
